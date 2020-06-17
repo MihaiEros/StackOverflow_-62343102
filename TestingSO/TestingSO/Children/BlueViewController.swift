2164909f7ea0cf6db0c40008555017635941794d
Mihai Erős <erosmihai92@gmail.com>
2020-06-17 14:05:29 +0300

    initial commit

---
+++ TestingSO/TestingSO/Children/BlueViewController.swift
@@ -0,0 +1,25 @@
+//
+//  BlueViewController.swift
+//  TestingSO
+//
+//  Created by Mihai Erős on 17/06/2020.
+//  Copyright © 2020 Mihai Eros. All rights reserved.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+    
+    @IBOutlet weak var navigationBar: UINavigationBar!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        navigationBar.isHidden = false
+        super.viewWillAppear(animated)
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationBar.isHidden = true
+        super.viewWillDisappear(animated)
+    }
+    
+}
